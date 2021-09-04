62b4cad16a656f7e23756560d82f76302de10af5
nghianguyen119 <trongnghia.nguyen@outlook.com>
2021-09-04 22:42:09 +0200

    * Adding dataIdOnchain * Adding dataRegisterProof

--- contracts/UniversalNFT.sol
+++ contracts/UniversalNFT.sol
@@ -12,26 +12,26 @@ contract UniversalNFT is ERC721URIStorage {
     Counters.Counter private _tokenIds;
 
     // Token CID card onchain
-    mapping(uint256 => string) private _tokenCIDs;
+    mapping(uint256 => string) private _dataIdOnchains;
 
     // Token dataRegisterProof onchain
-    mapping(uint256 => string) private _tokenDataProofs;
+    mapping(uint256 => string) private _dataRegisterProofs;
 
     constructor() ERC721("Universal NFT", "UNS") {}
 
-    function tokenCID(uint256 tokenId) public view returns (string memory) {
+    function getDataIdOnchain(uint256 tokenId) public view returns (string memory) {
         require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
 
-        string memory _tokenCID = _tokenCIDs[tokenId];
+        string memory _dataIdOnchain = _dataIdOnchains[tokenId];
         string memory base = _baseURI();
 
         // If there is no base URI, return the token URI.
         if (bytes(base).length == 0) {
-            return _tokenCID;
+            return _dataIdOnchain;
         }
         // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
-        if (bytes(_tokenCID).length > 0) {
-            return string(abi.encodePacked(base, _tokenCID));
+        if (bytes(_dataIdOnchain).length > 0) {
+            return string(abi.encodePacked(base, _dataIdOnchain));
         }
 
         // TODO: return when base != "", _tokenCID == ""
@@ -39,12 +39,35 @@ contract UniversalNFT is ERC721URIStorage {
         return "";
     }
 
-    function _setTokenCID(uint256 tokenId, string memory _tokenCID) internal {
-        require(_exists(tokenId), "ERC721URIStorage: CID set of nonexistent token");
-        _tokenCIDs[tokenId] = _tokenCID;
+    function getDataRegisterProof(uint256 tokenId) public view returns (string memory) {
+        require(_exists(tokenId), "ERC721URIStorage: URI query for nonexistent token");
+
+        string memory _dataRegisterProof = _dataRegisterProofs[tokenId];
+        string memory base = _baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _dataRegisterProof;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_dataRegisterProof).length > 0) {
+            return string(abi.encodePacked(base, _dataRegisterProof));
+        }
+
+        // TODO: return when base != "", _tokenCID == ""
+        // return super.tokenURI(tokenId);
+        return "";
     }
 
-    function mintUniversalNTF(address player, string memory tokenURI, string memory cardCID)
+    function _setDataIdOnchain(uint256 tokenId, string memory dataIdOnchain) internal {
+        _dataIdOnchains[tokenId] = dataIdOnchain;
+    }
+
+    function _setDataRegisterProof(uint256 tokenId, string memory dataRegisterProof) internal {
+        _dataRegisterProofs[tokenId] = dataRegisterProof;
+    }
+
+    function mintDataNTF(address player, string memory tokenURI, string memory dataIdOnchain, string memory dataRegisterProof)
         public
         returns (uint256)
     {
@@ -53,7 +76,8 @@ contract UniversalNFT is ERC721URIStorage {
         uint256 newItemId = _tokenIds.current();
         _mint(player, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        _setTokenCID(newItemId, cardCID);
+        _setDataIdOnchain(newItemId, dataIdOnchain);
+        _setDataRegisterProof(newItemId, dataRegisterProof);
 
         return newItemId;
     }
