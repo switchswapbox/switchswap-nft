582c14093a1d4bf10535f7cffd048db5b1cfb815
Huy Dung NGUYEN <huydung.ng.179@gmail.com>
2021-09-27 22:37:50 +0200

    deploy UniversalNFTV3

--- contracts/UniversalNFTV3.sol
+++ contracts/UniversalNFTV3.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract UniversalNFT is
+contract UniversalNFTV3 is
     ERC721URIStorage,
     ERC721Enumerable
 {
